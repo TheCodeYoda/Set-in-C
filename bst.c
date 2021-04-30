0b6ada59ed6bef4a8c8fd8e54cbfe41153987cd5
TheCodeYoda <smurf61988@gmail.com>
2021-04-30 11:19:01 +0530

    interface segregation and clang-format

--- bst.c
+++ bst.c
@@ -1,54 +1,68 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include"bst.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "bst.h"
 
-
-Node_t* create_node(int data) {
-    Node_t* node = malloc(sizeof(Node_t));
-    node->data = data;
-    node->left = NULL;
-    node->right = NULL;
+Node_t *create_node(int data)
+{
+  Node_t *node = malloc(sizeof(Node_t));
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
 }
 
-
-Node_t* insert(Node_t* root, int data) {
-    if(root==NULL) {
-	return create_node(data);
-    }
-    if(data<root->data)
-	root->left = insert(root->left,data);
-    else if(data>root->data)
-	root->right = insert(root->right,data);
-    return root;
-    
+Node_t *insert_tree(Node_t *root, int data)
+{
+  if (root == NULL) {
+    return create_node(data);
+  }
+  if (data < root->data) {
+    root->left = insert_tree(root->left, data);
+  }
+  else if (data > root->data) {
+    root->right = insert_tree(root->right, data);
+  }
+  return root;
 }
 
-void init_root(BST_t* tree) {
-    tree->root=NULL;
+void insert(Tree_t *tree, int data)
+{
+  if (tree->root == NULL) {
+    tree->root = insert_tree(tree->root, data);
+  }
+  else {
+    insert_tree(tree->root, data);
+  }
 }
 
-void inorder(Node_t* node) {
-    if(node==NULL)
-	return;
-    inorder(node->left);
-    printf("%d ",node->data);
-    inorder(node->right);
+void init_set(Tree_t *tree)
+{
+  tree->root = NULL;
 }
 
-void traverse(BST_t* tree) {
-    inorder(tree->root);
+void inorder(Node_t *node)
+{
+  if (node == NULL)
+    return;
+  inorder(node->left);
+  printf("%d ", node->data);
+  inorder(node->right);
 }
 
-int main() {
-    BST_t* tree = malloc(sizeof(BST_t));
-    init_root(tree);
-    tree->root = insert(tree->root,5);
-    insert(tree->root,9);
-    insert(tree->root,15);
-    insert(tree->root,1);
-    insert(tree->root,2);
-    insert(tree->root,2);
-    insert(tree->root,4);
-    traverse(tree);
-    
+void disp(Tree_t *tree)
+{
+  inorder(tree->root);
+}
+
+int main()
+{
+  Tree_t *tree = malloc(sizeof(Tree_t));
+  init_set(tree);
+  insert(tree, 5);
+  insert(tree, 9);
+  insert(tree, 15);
+  insert(tree, 1);
+  insert(tree, 2);
+  insert(tree, 2);
+  insert(tree, 4);
+  disp(tree);
 }
--- bst.h
+++ bst.h
@@ -1,18 +1,24 @@
 #ifndef BST_H
 #define BST_H
 typedef struct Node {
-    int data;
-    struct Node* left;
-    struct Node* right;
-}Node_t;
+  int data;
+  struct Node *left;
+  struct Node *right;
+} Node_t;
 
 typedef struct BST {
-    Node_t* root;
-}BST_t;
+  Node_t *root;
+} Tree_t;
 
-Node_t* create_node(int data);
-Node_t* insert(Node_t* tree,int data);
-void inorder(Node_t* node);
-void init_root(BST_t* tree);
-void traverse(BST_t* tree);
+/* Node interface */
+Node_t *create_node(int data);
+Node_t *insert_tree(Node_t *root, int data);
+void inorder(Node_t *root);
+void balance(Node_t *root);
+
+/* tree(set) interface */
+void init_set(Tree_t *tree);
+void disp(Tree_t *tree);
+void insert(Tree_t *tree, int data);
+
 #endif
