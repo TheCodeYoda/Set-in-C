d7a1aa565157d1a6f469624af06ce717a046f5ca
TheCodeYoda <smurf61988@gmail.com>
2021-04-30 20:17:33 +0530

    Iterator interface and balancing in progress

--- bst.c
+++ bst.c
@@ -8,6 +8,16 @@ int max(int a, int b)
   return (a > b) ? a : b;
 }
 
+Node_t *inorder_successor(Node_t *node)
+{
+  Node_t *temp = node;
+  temp = temp->right;
+  while (temp->left) {
+    temp = temp->left;
+  }
+  return temp;
+}
+
 Node_t *create_node(int data)
 {
   Node_t *node = malloc(sizeof(Node_t));
@@ -64,6 +74,7 @@ static Node_t *rightRotate(Node_t *y)
 // A utility function to left rotate subtree rooted with x
 static Node_t *leftRotate(Node_t *x)
 {
+  /* printf("L rotating %d\n", x->data); */
   Node_t *y = x->right;
   Node_t *T2 = y->left;
 
@@ -113,13 +124,13 @@ Node_t *insert_tree(Node_t *root, int data, int (*predicate)(int, int))
 
   /* LR case */
   if (balance > 1 && predicate(data, root->left->data) == 0) {
-    root->left = leftRotate(root);
+    root->left = leftRotate(root->left);
     return rightRotate(root);
   }
 
   /* RL case */
   if (balance < -1 && predicate(data, root->right->data) == 1) {
-    root->right = rightRotate(root);
+    root->right = rightRotate(root->right);
     return leftRotate(root);
   }
 
@@ -134,6 +145,87 @@ void insert(Tree_t *tree, int data)
   else {
     tree->root = insert_tree(tree->root, data, tree->predicate);
   }
+}
+
+Node_t *erase_tree(Node_t *root, int data, int (*predicate)(int, int))
+{
+  if (root == NULL) {
+    return NULL;
+  }
+  if (predicate(data, root->data) == 1) {
+    root->left = erase_tree(root->left, data, predicate);
+  }
+  else if (predicate(data, root->data) == 0) {
+    root->right = erase_tree(root->right, data, predicate);
+  }
+  else {
+    if ((root->left == NULL) || (root->right == NULL)) {
+      struct Node *temp = root->left ? root->left : root->right;
+
+      // No child case
+      if (temp == NULL) {
+        temp = root;
+        root = NULL;
+      }
+      else {  // One child case
+        *root = *temp;
+      }  // Copy the contents of
+         // the non-empty child
+      free(temp);
+    }
+    else {
+      // node with two children: Get the inorder
+      // successor (smallest in the right subtree)
+      struct Node *temp = inorder_successor(root);
+
+      // Copy the inorder successor's data to this node
+      root->data = temp->data;
+
+      // Delete the inorder successor
+      root->right = erase_tree(root->right, temp->data, predicate);
+    }
+  }
+
+  if (root == NULL) {
+    return root;
+  }
+
+  /* balancing here */
+
+  /* updation of ht */
+  root->ht = 1 + max(height(root->left), height(root->right));
+
+  /* compute balance factor */
+  int balance = get_balance_factor(root);
+
+  /* LL case */
+  if (balance > 1 && get_balance_factor(root->left) >= 0) {
+    return rightRotate(root);
+  }
+
+  /* LR case */
+  if (balance > 1 && get_balance_factor(root->left) < 0) {
+    root->left = leftRotate(root->left);
+    return rightRotate(root);
+  }
+
+  /* RR case */
+  if (balance < -1 && get_balance_factor(root->right) <= 0) {
+    return leftRotate(root);
+  }
+
+  /* RL case */
+  if (balance < -1 && get_balance_factor(root->right) > 0) {
+    root->right = rightRotate(root->right);
+    return leftRotate(root);
+  }
+
+  return root;
+}
+
+void erase(Tree_t *tree, int data)
+{
+  tree->root = erase_tree(tree->root, data, tree->predicate);
 }
 
 /* try to make it variable args */
@@ -155,6 +247,22 @@ void inorder(Node_t *node)
 void disp(Tree_t *tree)
 {
   inorder(tree->root);
+}
+
+static void tree_size(Node_t *node, int *num)
+{
+  if (node == NULL)
+    return;
+  *num += 1;
+  tree_size(node->left, num);
+  tree_size(node->right, num);
+}
+
+int size(Tree_t *tree)
+{
+  int num = 0;
+  tree_size(tree->root, &num);
+  return num;
 }
 
 int default_predicate(int a, int b)
@@ -179,23 +287,119 @@ int my_predicate(int a, int b)
   return -1;
 }
 
+/* Iterator interface */
+
+void init_iterator(Iterator_t *iter, Tree_t *tree)
+{
+  iter->ptr = tree->root;
+}
+
+static Iterator_t *create_iterator(Node_t *node)
+{
+  Iterator_t *temp = malloc(sizeof(Iterator_t));
+  temp->ptr = node;
+  return temp;
+}
+
+static int get_data(Iterator_t *it)
+{
+  if (it->ptr) {
+    return it->ptr->data;
+  }
+}
+
+Iterator_t *begin(Tree_t *tree)
+{
+  Node_t *start = tree->root;
+  while (start->left != NULL) {
+    start = start->left;
+  }
+  Iterator_t *iter = create_iterator(start);
+  return iter;
+}
+
+Iterator_t *end(Tree_t *tree)
+{
+  Node_t *start = tree->root;
+  while (start->right != NULL) {
+    start = start->right;
+  }
+  Iterator_t *iter = create_iterator(start);
+  return iter;
+}
+
+static Iterator_t *find_tree(Node_t *node, int data)
+{
+  if (node == NULL) {
+    return create_iterator(NULL);
+  }
+  if (data < node->data) {
+    return find_tree(node->left, data);
+  }
+  else if (data > node->data) {
+    return find_tree(node->right, data);
+  }
+  else {
+    return create_iterator(node);
+  }
+}
+
+Iterator_t *find(Tree_t *tree, int data)
+{
+  return find_tree(tree->root, data);
+}
+
+Iterator_t *next(Node_t *node)
+{
+  /* TODO */
+}
+
 int main()
 {
   Tree_t *tree = malloc(sizeof(Tree_t));
   init_set(tree, default_predicate);
+  insert(tree, 1);
+  insert(tree, 3);
   insert(tree, 5);
-  insert(tree, 9);
-  insert(tree, 15);
+  insert(tree, 7);
   insert(tree, 1);
   insert(tree, 2);
-  /* insert(tree, 2); */
+  insert(tree, 2);
+  insert(tree, 4);
+  printf("Size : %d\n", size(tree));
+
+  erase(tree, 2);
+  erase(tree, 4);
+  erase(tree, 7);
+
+  printf("Size : %d\n", size(tree));
+
+  insert(tree, 2);
+  insert(tree, 2);
   insert(tree, 4);
 
-  printf("%d\n", tree->root->data);
-  printf("%d\n", tree->root->left->data);
-  printf("%d\n", tree->root->right->data);
+  printf("Size : %d\n", size(tree));
+  /* printf("%d\n", tree->root->data); */
+  /* printf("%d\n", tree->root->left->data); */
+  /* printf("%d\n", tree->root->right->data); */
   /* printf("%d\n", tree->root->left->left->data); */
   /* printf("%d\n", tree->root->left->right->data); */
   /* printf("%d\n", tree->root->left->right->right->data); */
   disp(tree);
+
+  Iterator_t *it_start;
+  it_start = begin(tree);
+  printf("iter_start : %d\n", get_data(it_start));
+
+  Iterator_t *it_end;
+  it_end = end(tree);
+  printf("iter_start : %d\n", get_data(it_end));
+
+  Iterator_t *res = find(tree, 7);
+  if (res->ptr == NULL) {
+    printf("\n not found!!");
+  }
+  else {
+    printf("res : %d\n", get_data(res));
+  }
 }
--- bst.h
+++ bst.h
@@ -12,12 +12,17 @@ typedef struct BST {
   int (*predicate)(int, int);
 } Tree_t;
 
+typedef struct Iterator {
+  Node_t *ptr;
+} Iterator_t;
+
 /* Node interface */
 Node_t *create_node(int data);
-Node_t *insert_tree(Node_t *root, int data, int (*predicate)());
+Node_t *insert_tree(Node_t *root, int data, int (*predicate)(int, int));
+Node_t *erase_tree(Node_t *root, int data, int (*predicate)(int, int));
 void inorder(Node_t *root);
 void balance(Node_t *root);
-Node_t *next(Node_t *node); /* necessory?? supporting iterators?? */
+/* necessory?? supporting iterators?? */
 
 /* tree(set) interface */
 void init_set(Tree_t *tree, int (*predicate)());
@@ -25,7 +30,12 @@ void disp(Tree_t *tree);
 void insert(Tree_t *tree, int data);
 void erase(Tree_t *tree, int data);
 void clear(Tree_t *tree);
-int count(Tree_t *tree);
-Node_t *find(Tree_t *tree, int data);
 int size(Tree_t *tree);
+
+/* Iterator interface */
+void init_iterator(Iterator_t *iter, Tree_t *tree);
+Iterator_t *find(Tree_t *tree, int data);
+Iterator_t *begin(Tree_t *tree);
+Iterator_t *end(Tree_t *tree);
+Iterator_t *next(Node_t *node);
 #endif
